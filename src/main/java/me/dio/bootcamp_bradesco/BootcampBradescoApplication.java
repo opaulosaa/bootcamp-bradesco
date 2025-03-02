c60f3844012e6d322bc9659eab22a90297eab460
opaulosaa <pauloarneto@gmail.com>
2025-03-02 18:12:50 -0300

    Initial commit

---
+++ src/main/java/me/dio/bootcamp_bradesco/BootcampBradescoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.bootcamp_bradesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampBradescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampBradescoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/bootcamp_bradesco/BootcampBradescoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bootcamp_bradesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampBradescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
